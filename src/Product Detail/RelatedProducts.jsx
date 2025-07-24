11792243983b3484876882a1e8e636ea172e1b10
Ava-Hz <88708482+Ava-Hz@users.noreply.github.com>
2025-07-24 13:59:11 +0330

    testing

--- src/Product Detail/RelatedProducts.jsx
+++ src/Product Detail/RelatedProducts.jsx
@@ -74,7 +74,7 @@ const RandomProducts = () => {
               <img
                 src={item.Image?.url}
                 alt={item.plant_name}
-                className="w-[70%] mx-auto object-cover rounded mb-2"
+                className="w-full h-60 object-cover rounded mb-2"
                 loading="lazy"
               />
               <h3 className="text-lg font-bold">{item.plant_name}</h3>
